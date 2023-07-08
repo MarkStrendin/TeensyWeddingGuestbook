f13f0c273ec4529663f7e8b68b310c134495017a
Mark Strendin <mark.strendin@lskysd.ca>
2023-07-08 10:08:32 -0600

    Tidy up some more code and add comments to make the code easier to understand and therefore easier to maintain and debug

--- src/TeensyWeddingRecorder/TeensyWeddingRecorder.ino
+++ src/TeensyWeddingRecorder/TeensyWeddingRecorder.ino
@@ -6,6 +6,7 @@
 // Convert mp3s to wav files using this website because
 // audacity files don't seem to work
 // https://cloudconvert.com/mp3-to-wav
+// Files must be pcm16 and  44100Hz or they will not play
 
 // Includes
 #include <Audio.h>
@@ -23,10 +24,18 @@
 // Pin for the indicator LED
 #define LED_PIN 13
 
+// Set the master audio output volume
+#define AUDIO_OUTPUT_VOLUME 0.75
+
 // How loud should the beeps be
+// You should probably leave this at 0.1 so you don't cause hearing damage
+// to people with the speaker right up to their ear.
 #define BEEP_VOLUME 0.1
 
+#define BEEP_FREQUENCY 825
+
 // Which pins to use for SDCard bits
+// You probably don't want to mess with these
 #define SDCARD_CS_PIN    10
 #define SDCARD_MOSI_PIN  7
 #define SDCARD_SCK_PIN   14
@@ -41,32 +50,45 @@ Mode system_mode = Mode::Init;
 AudioControlSGTL5000 audio_shield_board;
 
 // Set up audio path for playback
+// This object represents the 3.5mm jack on the Teensy audio shield
 AudioOutputI2S audio_jack;
 
-// Set up audio path for recording
-AudioInputI2S  shield_mic;
-
 // Set up audio path for beeps
-AudioSynthWaveform    sndBeep;
+// This object can make waveforms (tones at specific frequencies).
+// We'll use it to make beeps.
+AudioSynthWaveform sndWaveFormCreator;
 
 // Set up audio file for the prompt
-AudioPlaySdWav sndGreeting;
+// This object can play arbitrary wav files off the SD card.
+// It can be re-used, so we only need the one.
+AudioPlaySdWav sndWavFilePlayer;
 
-// Set up virtual audio cables
+// Set up audio path for recording
+// This object represents the microphone standoffs on the Teensy audio shield
+AudioInputI2S mic_jack;
+
+// Set up virtual audio stack
+// We need a mixer to accept and combine multiple inputs (wav file and beeps)
 AudioMixer4 audioMixer;
-AudioConnection patchCord_mixer_out_1(audioMixer, 0, audio_jack, 0);
-AudioConnection patchCord_mixer_out_2(audioMixer, 0, audio_jack, 1);
 
-AudioConnection patchCord1(sndBeep, 0, audioMixer, 0);
-AudioConnection patchCord4(sndGreeting, 0, audioMixer, 1);
+// Hook the mixer to the audio jack L and R
+// Since this is wired to a telephone handset with only one speaker, we only need
+// one of these actually connected.
+//AudioConnection patchCord_mixer_out_left(audioMixer, 0, audio_jack, 0); // Left channel
+AudioConnection patchCord_mixer_out_right(audioMixer, 0, audio_jack, 1); // Right channel
 
+// Hook the wave form creator to the mixer, on a new channel
+AudioConnection patchCord_waveform_to_mixer(sndWaveFormCreator, 0, audioMixer, 0);
 
+// Hook the wav file player to the mixer, on a new channel
+AudioConnection patchCord4_wavfile_to_mixer(sndWavFilePlayer, 0, audioMixer, 1);
 
 
 // ***********************************************************
 // * Functions
 // ***********************************************************
 
+// Check to see if the phone is off hook by checking the status of the hook switch
 boolean is_phone_off_hook() {
   if (digitalRead(HOOK_BUTTON_PIN) == LOW) {
     return true;
@@ -74,22 +96,26 @@ boolean is_phone_off_hook() {
     return false;
   }
 }
+
+// Play a beep to indicate that recording has started
 void beep_start_recording() {
-  sndBeep.begin(BEEP_VOLUME, 825, WAVEFORM_SINE);
-  delay(800);
-  sndBeep.amplitude(0);
+  sndWaveFormCreator.begin(BEEP_VOLUME, BEEP_FREQUENCY, WAVEFORM_SINE);
+  delay(750);
+  sndWaveFormCreator.amplitude(0);
 }
 
+// Play  a beep to indicate that recording has finished
 void beep_end_recording() {
-  sndBeep.begin(BEEP_VOLUME, 825, WAVEFORM_SINE);
-  delay(100);
-  sndBeep.amplitude(0);
-  delay(100);
-  sndBeep.begin(BEEP_VOLUME, 825, WAVEFORM_SINE);
-  delay(100);
-  sndBeep.amplitude(0);
+  for(int x=0;x<3;x++) {
+    sndWaveFormCreator.begin(BEEP_VOLUME, BEEP_FREQUENCY, WAVEFORM_SINE);
+    delay(75);
+    sndWaveFormCreator.amplitude(0);  
+    delay(75);
+  }
 }
 
+// Play an arbitrary wav file
+// File must be pcm16 and  44100Hz or it will not play
 void playFile(const char *filename)
 {
   Serial.print("Playing file: ");
@@ -97,20 +123,22 @@ void playFile(const char *filename)
 
   // Start playing the file.  This sketch continues to
   // run while the file plays.
-  sndGreeting.play(filename);
+  sndWavFilePlayer.play(filename);
 
   // A brief delay for the library read WAV info
   delay(5);
 
   // Simply wait for the file to finish playing.
-  while (sndGreeting.isPlaying()) {
+  while (sndWavFilePlayer.isPlaying()) {
     if (!is_phone_off_hook()) {
-      sndGreeting.stop();
+      sndWavFilePlayer.stop();
       break;
     }
   }
 }
 
+// Return a string indicated what "mode" the system is in
+// so we can display it on the serial connection
 String get_system_mode() {
   if (system_mode == Mode::Init) { return "Init"; }
   if (system_mode == Mode::Ready) { return "Ready"; }
@@ -129,21 +157,22 @@ void setup() {
   // Set up indictor LED
   pinMode(LED_PIN, OUTPUT);
 
-  // set up audio system
+  // allocate some memory for the audio system
+  // https://www.pjrc.com/teensy/td_libs_AudioConnection.html
+  // This number is memory blocks, each which holds 128 audio samples, or approx 2.9ms of sound
   AudioMemory(60);
   
+  // Set the volume on the mixer inputs to max
   audioMixer.gain(0, 1.0f);
   audioMixer.gain(1, 1.0f);
 
   audio_shield_board.enable();
-  audio_shield_board.volume(1.0);
   audio_shield_board.inputSelect(AUDIO_INPUT_MIC);
   //audio_shield_board.adcHighPassFilterDisable();
-  audio_shield_board.volume(0.75);
+  audio_shield_board.volume(AUDIO_OUTPUT_VOLUME);
 
   // Set up on-hook button
   pinMode(HOOK_BUTTON_PIN, INPUT_PULLUP);
-
 
   // Set up SD card
   SPI.setMOSI(SDCARD_MOSI_PIN);
@@ -184,7 +213,6 @@ void loop() {
   // See if we need to change the state of the system
 
   Serial.println(get_system_mode() + " : " + get_hook_status());
-
   
   switch(system_mode) {
     case Mode::Init: 
@@ -231,9 +259,7 @@ void loop() {
           system_mode = Mode::Recording;
         } else {
           system_mode = Mode::Ready;
-        }
-
-        
+        }        
       } else {
         system_mode = Mode::Ready;
       }
